3bf9b4f83f2e5e11df38d60e8c3fd27a92d4f577
David Gracia <david.gracia.celemendi@gmail.com>
2018-11-15 19:29:46 +0100

    removed dead code

--- src/main/java/com/codurance/top_gear_kata/GearBox.java
+++ src/main/java/com/codurance/top_gear_kata/GearBox.java
@@ -29,24 +29,19 @@ public class GearBox {
     private int e = 0;
 
     public void doit(int rpm) {
-        if (gear < 0) {
-            // do nothing!
-            e = rpm;
-        } else {
-            if (gear > 0) {
-                if (rpm > 2000) {
-                    gear++;
-                } else if (rpm < 500) {
-                    gear--;
-                }
-            }
-            if (gear > 6) {
-                gear--;
-            } else if (gear < 1) {
+        if (gear > 0) {
+            if (rpm > 2000) {
                 gear++;
+            } else if (rpm < 500) {
+                gear--;
             }
-            e = rpm;
         }
+        if (gear > 6) {
+            gear--;
+        } else if (gear < 1) {
+            gear++;
+        }
+        e = rpm;
     }
 
     public int gear() {
