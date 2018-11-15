227e4882cb7bda748aed3fbdae781c8d23d093d3
David Gracia <david.gracia.celemendi@gmail.com>
2018-11-15 18:36:26 +0100

    formatting

--- src/main/java/com/codurance/top_gear_kata/GearBox.java
+++ src/main/java/com/codurance/top_gear_kata/GearBox.java
@@ -25,27 +25,28 @@ package com.codurance.top_gear_kata;
 
 public class GearBox {
 
-	private int gear = 0;
-	private int e = 0;
+    private int gear = 0;
+    private int e = 0;
 
-	public void doit(int rpm) {
-		if (gear < 0) {
-			// do nothing!
-			e = rpm;
-		} else {
-			if (gear > 0) {
-				if (rpm > 2000)
-					gear++;
-				} else if (rpm < 500) {
-					gear--;
-				}
-			} if (gear > 6) {
-				gear--;
-			} else if (gear < 1) {
-				gear++;
-			}
-			e = rpm;
-		}
+    public void doit(int rpm) {
+        if (gear < 0) {
+            // do nothing!
+            e = rpm;
+        } else {
+            if (gear > 0) {
+                if (rpm > 2000)
+                    gear++;
+            } else if (rpm < 500) {
+                gear--;
+            }
+        }
+        if (gear > 6) {
+            gear--;
+        } else if (gear < 1) {
+            gear++;
+        }
+        e = rpm;
+    }
 
     public int gear() {
         return gear;
